63feb1728d00c060b68fa42a19c4103895f5e4d5
TatsukiMinami <tatsukiminami@TatsukiMinaminoMacBook-Pro.local>
2018-01-16 21:09:05 +0900

    用紙の送信周りの追加

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '用紙選択' });
 });
-
+router.get('/login', function(req, res, next){
+  res.render('login',{title: 'ログイン画面'})
+});
+router.post('/', function(req, res, next){
+  var pepar = req.body.pepar;
+  var floar = req.body.floar;
+  var hoge = req.body.class;
+  var name = req.body.name;
+  var createAt = moment().format('YYYY-MM-DD HH:mm:ss');
+  
+})
 module.exports = router;
