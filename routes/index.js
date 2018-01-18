53c9536f793758f287641d83fb38bf1ebf2f8857
TatsukiMinami <tatsukiminami@TatsukiMinaminoMacBook-Pro.local>
2018-01-19 04:12:43 +0900

    ログイン登録画面周りの追加

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,23 @@ var moment = require('moment');
 router.get('/', function(req, res, next) {
   res.render('index', { title: '用紙選択' });
 });
+
 router.get('/login', function(req, res, next){
   res.render('login',{title: 'ログイン画面'})
 });
+
+router.get('/register', function(req, res, next){
+  res.render('register',{title: 'ユーザー登録画面'})
+});
+
 router.post('/', function(req, res, next){
-  var pepar = req.body.pepar;
+  var paper = req.body.paper;
   var floar = req.body.floar;
   var hoge = req.body.class;
   var name = req.body.name;
   var createAt = moment().format('YYYY-MM-DD HH:mm:ss');
   
-})
+});
+
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/login', function(req, res, next){
+  var id = req.body.id;
+  var password = req.body.password;
+});
+
+router.post('/register', function(req, res, next){
+  var id = req.body.id;
+  var name = req.body.name;
+  var class_number = req.body.class_number;
+  var password = req.body.password;
+  console.log(id);
+  console.log(name);
+  console.log(class_number);
+  console.log(password);
+});
 module.exports = router;
