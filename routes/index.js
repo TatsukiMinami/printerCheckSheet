ea883d8b74c177d9bc0f9f83a3e31af068a82f33
TatsukiMinami <tatsukiminami@TatsukiMinaminoMacBook-Pro.local>
2018-01-21 19:48:23 +0900

    細かい所の修正

--- routes/index.js
+++ routes/index.js
@@ -8,21 +8,47 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login', function(req, res, next){
-  res.render('login',{title: 'ログイン画面'})
+  res.render('login', {title: 'ログイン画面'})
 });
 
 router.get('/register', function(req, res, next){
-  res.render('register',{title: 'ユーザー登録画面'})
+  res.render('register', {title: '登録画面'})
 });
 
+router.get('/list', function(req, res, next){
+  var query = 'SELECT *, DATE_FORMAT(created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM paper';
+  connection.query(query, function(err, row){
+    res.render('list',{
+      title: '取引一覧',
+      paperList: row
+    });
+  });
+});
+
+router.post('/login', function(req, res, next){
+  var id = req.body.id;
+  var password = req.body.password;
+});
+
+router.post('/register', function(req, res, next){
+  var id = req.body.id;
+  var name = req.body.name;
+  var class_number = req.body.class_number;
+  var password = req.body.password;
+  var generation = req.body.generation
+  var query = 'INSERT INTO user (id, name, class_number, password, generation) VALUES("' + id + '", ' + '"' + name + '", ' + '"' 
+           + class_number + '", ' + '"' + password + '", ' + '"' + generation + '")'; 
+  createConnection.query(query, function(err,rows){
+    res.redirect('/');
+  });
+});
 router.post('/', function(req, res, next){
-  var paper = req.body.paper;
+  var type = req.body.type;
   var floar = req.body.floar;
   var hoge = req.body.class;
   var name = req.body.name;
-  var createAt = moment().format('YYYY-MM-DD HH:mm:ss');
-  
+  var create_at = moment().format('YYYY-MM-DD HH:mm:ss');
+  var query = 'INSERT INTO paper ()'
 });
-
 
 module.exports = router;
